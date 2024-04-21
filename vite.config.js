04099192ac63a6a1076c307faaecf715ceffcb99
iangwapo007 <iegeneralao@gmail.com>
2024-04-21 23:55:49 +0800

    try

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/main.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
+    build: {
+        css: {
+            output: 'main.css',
+        }
+    }
+});
